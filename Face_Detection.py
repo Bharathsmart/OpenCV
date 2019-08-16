d76ffe6dfb3b4153a364781c94655664d862ccc9
Bharathsmart <40082241+Bharathsmart@users.noreply.github.com>
2019-08-16 11:45:06 +0530

    Add files via upload
    Face and Eye Detection of a given image

---
+++ Face_Detection.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import cv2
+import numpy as np
+
+#create a cascade object for Face and Eye
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+
+#Image loading
+img = cv2.imread('image_path or image.jpg')
+
+#converts to gray scale
+gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+faces = face_cascade.detectMultiScale(gray,scaleFactor=1.05,minNeighbors=5)
+aces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+for (x,y,w,h) in faces:
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,0,0),2)
+    roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
+    eyes = eye_cascade.detectMultiScale(roi_gray)
+    for (ex,ey,ew,eh) in eyes:
+        cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,0,0),2) #Boxes for eyes
+
+cv2.imshow('lena',img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
